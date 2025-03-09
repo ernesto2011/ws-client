65c3381f3604da75a6f95f0703c20bee4042cea4
ernesto2011 <skillmx107@gmail.com>
2025-03-09 14:57:54 -0600

    :rocket:

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,35 @@ import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <input id="jwtToken" placeholder=" Json web token" />
-    <button id="connect">Connect</button>
-    <br />
-    <span id="status">OFFLINE</span>
+    <h2>Websocket - Client</h2>
+    <input id="jwt-token" placeholder="Json Web Token" />
+    <button id="btn-connect">Connect</button>
+
+    <br/>
+    <span id="server-status">offline</span>
+
     <ul id="clients-ul"></ul>
+
     <form id="message-form">
-      <input placeholder="Payload" id="message-input">
+      <input placeholder="message" id="message-input" />
     </form>
-    <h3>Mensajes</h3>
+
+
+    <h3>Messages</h3>
     <ul id="messages-ul"></ul>
+
+
   </div>
 `
+
 //connectToServer();
-const jwtToken = document.querySelector<HTMLInputElement>('#jwtToken')!
-const connectBtn = document.querySelector<HTMLButtonElement>('#connect')!
-connectBtn.addEventListener('click', () => {
-  if(jwtToken.value.trim().length <= 0) return alert('Enter a valid JWT')
-  connectToServer(jwtToken.value)
+const jwtToken = document.querySelector<HTMLInputElement>('#jwt-token')!;
+const btnConnect = document.querySelector<HTMLButtonElement>('#btn-connect')!;
+
+btnConnect.addEventListener('click', () => {
+
+  if ( jwtToken.value.trim().length <= 0 ) return alert('Enter a valid JWT');
+
+  connectToServer( jwtToken.value.trim() );
+
 })
